7833a884c9a420d492950d03ccb1b382936c5602
ander <soer9428@edu.easj.dk>
2021-08-18 00:55:45 +0200

    setup af footer

--- include/footer.php
+++ include/footer.php
@@ -1,2 +1,30 @@
 <link href="css/styles.css" rel="stylesheet" type="text/css">
 
+<footer class="footer__container flex-column">
+    <div class="footer__contact text-center">
+        <h3>Kontakt</h3>
+        <a href="">+45 24 42 78 92</a>
+        <br>
+        <a href="">andersen-s93@hotmail.com</a>
+    </div>
+
+    <div class="footer__social text-center">
+        <h3>Følg mig</h3>
+        <div class="footer__social__item d-flex justify-content-center">
+        <a class="">
+            <img class="footer__social__icon" src="img/001-linkedin.svg" alt="linkedin profil">
+        </a>
+        <a class="">
+            <img class="footer__social__icon" src="img/001-linkedin.svg" alt="?">
+        </a>
+        </div>
+    </div>
+
+    <div class="footer__nav text-center text-white">
+        <h3>Menu</h3>
+        <a class="text-secondary fs-5" href="index.php">Forside</a>
+        <a class="text-secondary fs-5" href="kompetencer.php">Kompetencer</a>
+        <a class="text-secondary fs-5" href="ommig.php">Om mig</a>
+        <a class="text-secondary fs-5" href="kontakt.php">Kontakt</a>
+    </div>
+</footer>
