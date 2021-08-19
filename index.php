6482e8817b06621beaa1de179cb3584a2fef810c
ander <soer9428@edu.easj.dk>
2021-08-19 15:39:44 +0200

    forside med main, hvor profil billede og velkomst text og section som teaser på hvad jeg kan via cards. er fuld responsiv. Tilføjelse af billeder til de forskellige sider.

--- index.php
+++ index.php
@@ -30,7 +30,62 @@
 
 <?php include 'include/header.php';?>
 
-<div class="text-center">Text</div>
+<main class="main d-flex justify-content-evenly py-5">
+    <div class="main__text flex-md-column align-self-md-center text-white ps-2 pe-1">
+    <h3 class="text-xl-center mb-4">Hej med dig</h3>
+        <p class="text-xl-center">Jeg hedder Søren og studere til Multimediedesign på Zealand.
+            <br><br>
+            Website, grafisk design, UX/UI.
+            <br><br>
+            Se hvad jeg kan.</p>
+        <div class="d-xl-flex justify-content-xl-center">
+            <a class="btn btn-primary mt-4" href="kompetencer.php" role="button">Klik her</a>
+        </div>
+    </div>
+
+    <div class="ps-1 pe-2">
+        <img class="main__image rounded" src="img/søren.jpg" alt="billede af Søren">
+    </div>
+</main>
+
+<section class="container mt-5 mb-4">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
+        <div class="col mb-3">
+            <div class="card mx-auto" style="width: 18rem;">
+                <img src="img/kaffetaarn.png" class="card-img-top" alt="billede af websit til KAFFETÅR'N">
+                <div class="card-body">
+                    <h5 class="card-title">Websits</h5>
+                    <p class="card-text">Se hvilke hjemmesider jeg har været med til at lave. Der er alt fra en t-shirt shop til en launch side for en musikvideo. </p>
+                    <a href="#" class="btn btn-primary">Klik her</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col mb-3">
+            <div class="card mx-auto" style="width: 18rem;">
+                <img src="img/moodboardmusikvideo.png" class="card-img-top" alt="moodboard til musikvideoprojekt">
+                <div class="card-body">
+                    <h5 class="card-title">Visuelt design</h5>
+                    <p class="card-text">Se hvad jeg kan, inden for visuelt design, hvor du finder en masse forskellig bl.a. logoer, tryk til t-shirt og meget mere.</p>
+                    <a href="#" class="btn btn-primary">Klik her</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col mb-3">
+            <div class="card mx-auto" style="width: 18rem;">
+                <img src="img/muckupslokalitee.PNG" class="card-img-top" alt="muckups til webshop LOKALITEE">
+                <div class="card-body">
+                    <h5 class="card-title">Prototyper</h5>
+                    <p class="card-text">Se hvordan jeg bygger et website op fra bunden via wireframe og mockups.</p>
+                    <a href="#" class="btn btn-primary">Klik her</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 
 <?php include 'include/footer.php';?>
 
